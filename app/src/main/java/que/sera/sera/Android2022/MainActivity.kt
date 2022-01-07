2892067932abe4c0cdfe51d093c1346db1372752
quesera2 <quesera2262@gmail.com>
2022-01-07 15:58:22 +0900

    最小限のプロジェクト構成を作成

---
+++ app/src/main/java/que/sera/sera/Android2022/MainActivity.kt
@@ -0,0 +1,18 @@
+package que.sera.sera.Android2022
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import que.sera.sera.Android2022.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/que/sera/sera/Android2022/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package que.sera.sera.Android2022.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
