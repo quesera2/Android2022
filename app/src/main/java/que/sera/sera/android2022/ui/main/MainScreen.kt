e5de14a2fe378719b06908a9033772b29c5464fe
quesera2 <quesera2262@gmail.com>
2022-01-11 17:22:30 +0900

    プレビューを追加
    ラッパー関数を作成するという地味な方法で実現する、その他にもPreviewParameterを渡す方法もある

--- app/src/main/java/que/sera/sera/android2022/ui/main/MainScreen.kt
+++ app/src/main/java/que/sera/sera/android2022/ui/main/MainScreen.kt
@@ -3,9 +3,17 @@ package que.sera.sera.android2022.ui.main
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     Text(text = stringResource(viewModel.greeting))
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    val viewModel = MainViewModel()
+    MainScreen(viewModel = viewModel)
 }
