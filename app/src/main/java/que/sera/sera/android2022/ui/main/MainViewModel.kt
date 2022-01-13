7a1375e2f99c1edc37f2edf66f8a7d6bdcb6d785
quesera2 <quesera2262@gmail.com>
2022-01-13 17:37:58 +0900

    ViewModelをHiltのインジェクションに対応

--- app/src/main/java/que/sera/sera/android2022/ui/main/MainViewModel.kt
+++ app/src/main/java/que/sera/sera/android2022/ui/main/MainViewModel.kt
@@ -1,9 +1,15 @@
 package que.sera.sera.android2022.ui.main
 
-import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
-import que.sera.sera.android2022.R
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import que.sera.sera.android2022.model.todo.ToDo
+import que.sera.sera.android2022.repository.todo.ToDoRepository
+import javax.inject.Inject
 
-class MainViewModel(
-    @StringRes val greeting: Int = R.string.greeting
-) : ViewModel()
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val toDoRepository: ToDoRepository
+) : ViewModel() {
+    fun getToDos(): Flow<List<ToDo>> = toDoRepository.getTodos()
+}
