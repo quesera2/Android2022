62b2879463bf0891144ad874caf0d2749a876596
quesera2 <quesera2262@gmail.com>
2022-01-13 17:37:10 +0900

    ActivityEntoryPointを追加

--- app/src/main/java/que/sera/sera/android2022/MainActivity.kt
+++ app/src/main/java/que/sera/sera/android2022/MainActivity.kt
@@ -3,16 +3,16 @@ package que.sera.sera.android2022
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import que.sera.sera.android2022.ui.main.MainScreen
-import que.sera.sera.android2022.ui.main.MainViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = MainViewModel()
         setContent {
-            MainScreen(viewModel)
+            MainScreen()
         }
     }
 }
