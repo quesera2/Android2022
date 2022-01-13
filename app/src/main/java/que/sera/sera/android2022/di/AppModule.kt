80c37726b124e3a4ea8e7092c258f066dc3ce4e3
quesera2 <quesera2262@gmail.com>
2022-01-13 17:37:23 +0900

    AppModuleにリポジトリを追加

--- app/src/main/java/que/sera/sera/android2022/di/AppModule.kt
+++ app/src/main/java/que/sera/sera/android2022/di/AppModule.kt
@@ -7,6 +7,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import que.sera.sera.android2022.repository.todo.ToDoRepository
+import que.sera.sera.android2022.repository.todo.ToDoRepositoryImpl
 import que.sera.sera.android2022.room.AppDatabase
 import javax.inject.Singleton
 
@@ -18,5 +20,12 @@ object AppModule {
     @Provides
     fun provideAppDatabase(
         @ApplicationContext context: Context
-    ) = Room.databaseBuilder(context, AppDatabase::class.java, "app_db").build()
+    ): AppDatabase = Room.databaseBuilder(context, AppDatabase::class.java, "app_db").build()
+
+
+    @Singleton
+    @Provides
+    fun provideToDoRepository(
+        appDatabase: AppDatabase
+    ): ToDoRepository = ToDoRepositoryImpl(appDatabase)
 }
