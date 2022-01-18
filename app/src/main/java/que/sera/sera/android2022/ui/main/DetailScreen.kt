543146b46ef5088756e024536fd4fd8fcd7dccf9
quesera2 <quesera2262@gmail.com>
2022-01-18 17:17:08 +0900

    ダミーの詳細画面を作成

---
+++ app/src/main/java/que/sera/sera/android2022/ui/main/DetailScreen.kt
@@ -0,0 +1,33 @@
+package que.sera.sera.android2022.ui.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun DetailScreen() {
+    Scaffold(
+        topBar = { AppBar() },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(onClick = { /* TODO */ }) {
+                    Text(text = "戻る")
+                }
+            }
+        }
+    )
+}
