2991aaedcd54dcf23584a684fe20069cdc8f9206
quesera2 <quesera2262@gmail.com>
2022-01-19 11:01:45 +0900

    色管理をxmlで統一

--- app/src/main/java/que/sera/sera/android2022/MainActivity.kt
+++ app/src/main/java/que/sera/sera/android2022/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import dagger.hilt.android.AndroidEntryPoint
 import que.sera.sera.android2022.ui.app.MyApp
-import que.sera.sera.android2022.ui.theme.AppTheme
 
 @ExperimentalMaterial3Api
 @AndroidEntryPoint
@@ -15,9 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                MyApp()
-            }
+            MyApp()
         }
     }
 }
--- app/src/main/java/que/sera/sera/android2022/ui/app/App.kt
+++ app/src/main/java/que/sera/sera/android2022/ui/app/App.kt
@@ -6,24 +6,27 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.material.composethemeadapter3.Mdc3Theme
 import que.sera.sera.android2022.ui.detail.DetailScreen
 import que.sera.sera.android2022.ui.main.MainScreen
 
 @ExperimentalMaterial3Api
 @Composable
 fun MyApp() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "main") {
-        composable("main") {
-            MainScreen(
-                viewModel = hiltViewModel(),
-                navController = navController
-            )
-        }
-        composable("detail") {
-            DetailScreen(
-                navController = navController
-            )
+    Mdc3Theme {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "main") {
+            composable("main") {
+                MainScreen(
+                    viewModel = hiltViewModel(),
+                    navController = navController
+                )
+            }
+            composable("detail") {
+                DetailScreen(
+                    navController = navController
+                )
+            }
         }
     }
 }
