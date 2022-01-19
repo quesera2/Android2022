2b7e6bdd5d393e0ec5dab1b5fb56fe77587fa874
quesera2 <quesera2262@gmail.com>
2022-01-19 14:41:23 +0900

    画面遷移のアニメーションを追加

--- app/src/main/java/que/sera/sera/android2022/MainActivity.kt
+++ app/src/main/java/que/sera/sera/android2022/MainActivity.kt
@@ -3,10 +3,13 @@ package que.sera.sera.android2022
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import dagger.hilt.android.AndroidEntryPoint
 import que.sera.sera.android2022.ui.app.MyApp
 
+
+@ExperimentalAnimationApi
 @ExperimentalMaterial3Api
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/que/sera/sera/android2022/ui/app/App.kt
+++ app/src/main/java/que/sera/sera/android2022/ui/app/App.kt
@@ -1,28 +1,45 @@
 package que.sera.sera.android2022.ui.app
 
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.android.material.composethemeadapter3.Mdc3Theme
 import que.sera.sera.android2022.ui.detail.DetailScreen
 import que.sera.sera.android2022.ui.main.MainScreen
+import kotlin.math.roundToInt
 
+@ExperimentalAnimationApi
 @ExperimentalMaterial3Api
 @Composable
 fun MyApp() {
     Mdc3Theme {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "main") {
-            composable("main") {
+        val navController = rememberAnimatedNavController()
+        val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+        AnimatedNavHost(
+            navController = navController,
+            startDestination = "main"
+        ) {
+            composable(
+                route = "main"
+            ) {
                 MainScreen(
                     viewModel = hiltViewModel(),
                     navController = navController
                 )
             }
-            composable("detail") {
+            composable(
+                route = "detail",
+                enterTransition = { slideInVertically(initialOffsetY = { screenHeight.value.roundToInt() }) },
+                exitTransition = { slideOutVertically(targetOffsetY = { screenHeight.div(2).value.roundToInt() }) + fadeOut() }
+            ) {
                 DetailScreen(
                     navController = navController
                 )
