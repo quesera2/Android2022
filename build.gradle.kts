c44adf4fa8e21739e9706ea90904d0cef5a1a30b
quesera2 <quesera2262@gmail.com>
2022-12-28 14:34:19 +0900

    classpathもlibsで書くように変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("com.google.gms:google-services:4.3.13")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+        classpath(libs.com.android.tools.build.gradle)
+        classpath(libs.org.jetbrains.kotlin.kotlin.gradle.plugin)
+        classpath(libs.com.google.gms.google.services)
+        classpath(libs.com.google.dagger.hilt.android.gradle.plugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
